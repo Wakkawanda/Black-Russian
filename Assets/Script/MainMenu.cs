9ab27901f4860dd9920e9ac713ec6072cb1fa864
Georgi <i22s0659@stydents.it-college.ru>
2024-02-18 02:34:20 +0500

    Autors

--- Assets/Script/MainMenu.cs
+++ Assets/Script/MainMenu.cs
@@ -11,56 +11,41 @@ using Button = UnityEngine.UI.Button;
 
 public class MainMenu : MonoBehaviour
 {
-    [SerializeField] private Button play, autors, exit, settings;
-    [SerializeField] private GameObject n1, n2, n3;
+    [SerializeField] private Button play, autors, exit, back;
+    [SerializeField] private GameObject n1, n2, autor, board;
     
     public void OnEnable()
     {   
         play.onClick.AddListener(() => SceneManager.LoadScene("Game"));
         exit.onClick.AddListener(Application.Quit);
         autors.onClick.AddListener(AutorsClick);
-        // back.onClick.AddListener(BackClick);
-        settings.onClick.AddListener(SettingsClick);
-    }
-
-    private void SettingsClick()
-    {
-        play.gameObject.SetActive(false);
-        autors.gameObject.SetActive(false);
-        settings.gameObject.SetActive(false);
-        exit.gameObject.SetActive(false);
-        n1.gameObject.SetActive(false);
-        n2.gameObject.SetActive(false);
-        n3.gameObject.SetActive(false);
-        
-        
+        back.onClick.AddListener(BackClick);
     }
 
     private void BackClick()
     {
         play.gameObject.SetActive(true);
         autors.gameObject.SetActive(true);
-        settings.gameObject.SetActive(true);
         exit.gameObject.SetActive(true);
         n1.gameObject.SetActive(true);
         n2.gameObject.SetActive(true);
-        n3.gameObject.SetActive(true);
+        board.gameObject.SetActive(true);
         
-        
-        
+        autor.gameObject.SetActive(false);
+        back.gameObject.SetActive(false);
     }
 
     private void AutorsClick()
     {
         play.gameObject.SetActive(false);
         autors.gameObject.SetActive(false);
-        settings.gameObject.SetActive(false);
         exit.gameObject.SetActive(false);
         n1.gameObject.SetActive(false);
         n2.gameObject.SetActive(false);
-        n3.gameObject.SetActive(false);
+        board.gameObject.SetActive(false);
         
-        
+        autor.gameObject.SetActive(true);
+        back.gameObject.SetActive(true);
     }
 
 
